e88e164f59b1a1d54ab716e929f31ca84b797c14
murphy <1243917465@qq.com>
2025-05-25 20:47:07 +0800

    更新 Tailwind 配置，将背景颜色从变量修改为白色；更新布局元数据标题为“拍立懂”。

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "#ffffff",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "#07c160",
